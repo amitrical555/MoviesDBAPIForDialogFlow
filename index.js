a5dba13a0b3986203ac085a2cada1909a3fecd26
amitrical555 <amitrical555@rediffmail.com>
2019-04-08 00:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ server.post('/get-movie-details', (req, res) => {
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
-                fullfillmentText: "Here are the details on " + movieToSearch,
-                fullfillmentMessages: [
+                fulfillmentText: "Here are the details on " + movieToSearch,
+                fulfillmentMessages: [
                     {
                         simpleResponses: {
                             simpleResponses: [
@@ -44,8 +44,8 @@ server.post('/get-movie-details', (req, res) => {
         });
     }, (error) => {
         return res.json({
-                fullfillmentText: "Here are the details on " + movieToSearch,
-                fullfillmentMessages: [
+                fulfillmentText: "Here are the details on " + movieToSearch,
+                fulfillmentMessages: [
                     {
                         simpleResponses: {
                             simpleResponses: [
